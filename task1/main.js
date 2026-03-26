bffff1731a98fe79e86f8fce472eeddf0f44f177
KhrystynaHrytsiv <khrystyna.hrytsiv@gmail.com>
2026-03-26 23:04:07 +0200

    task 1

---
+++ task1/main.js
@@ -0,0 +1,42 @@
+let form = document.forms[0];
+let infoBlock = document.getElementsByClassName('infoBlock')[0];
+let array = [];
+form.onsubmit = function (ev){
+    ev.preventDefault();
+    let input = form.keys.value;
+    let match = input.match(/^([a-zA-Z0-9]+)\s*=\s*([a-zA-Z0-9]+)$/);
+    if (match){
+        let name = match[1];
+        let value = match[2];
+        array.push({name, value});
+        show()
+    }else {
+        let error = document.getElementById('error');
+        error.style.display = 'block';
+    }
+}
+function show (){
+    infoBlock.innerHTML = '';
+    array.forEach(item =>{
+        let div = document.createElement('div');
+        div.innerText = `${item.name} = ${item.value}`;
+        infoBlock.appendChild(div);
+    })
+}
+
+let sortByNameButton = document.getElementById('sortByName');
+sortByNameButton.onclick = function (){
+    array.sort((a, b) => a.name.localeCompare(b.name));
+    show()
+};
+let sortByValueButton = document.getElementById('sortByValue');
+sortByValueButton.onclick = function (){
+    array.sort((a, b) => a.value.localeCompare(b.value));
+    show()
+};
+
+let deleteButton = document.getElementById('delete');
+deleteButton.onclick = function (){
+    infoBlock.innerText = ''
+    array = [];
+};
