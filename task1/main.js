ffe47b87a70b4578b8c967da4a436abe1950f172
KhrystynaHrytsiv <khrystyna.hrytsiv@gmail.com>
2026-04-06 19:57:31 +0300

    task 1 adaptive

--- task1/main.js
+++ task1/main.js
@@ -5,15 +5,17 @@ form.onsubmit = function (ev){
     ev.preventDefault();
     let input = form.keys.value;
     let match = input.match(/^([a-zA-Z0-9]+)\s*=\s*([a-zA-Z0-9]+)$/);
+    let error = document.getElementById('error');
     if (match){
         let name = match[1];
         let value = match[2];
         array.push({name, value});
+        error.style.display = 'none';
         show()
     }else {
-        let error = document.getElementById('error');
         error.style.display = 'block';
     }
+    form.reset()
 }
 function show (){
     infoBlock.innerHTML = '';
@@ -26,12 +28,12 @@ function show (){
 
 let sortByNameButton = document.getElementById('sortByName');
 sortByNameButton.onclick = function (){
-    array.sort((a, b) => a.name.localeCompare(b.name));
+    array.sort((a, b) => a.name - b.name);
     show()
 };
 let sortByValueButton = document.getElementById('sortByValue');
 sortByValueButton.onclick = function (){
-    array.sort((a, b) => a.value.localeCompare(b.value));
+    array.sort((a, b) => a.value - b.value);
     show()
 };
 
